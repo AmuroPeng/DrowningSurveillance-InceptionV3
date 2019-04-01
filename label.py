bf5f7eeb69390907ed5ecd76d80ca70d557f5462
AmuroPeng <tiggerpeng1997@gmail.com>
2019-04-02 01:05:35 +0800

    update

--- label.py
+++ label.py
@@ -4,18 +4,21 @@
 import tensorflow as tf
 import os
 from os.path import join as pjoin
+import json
 
 data_dir = r".\test"  # 测试集路径
 
-label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]  # 删除训练生成的txt文件最后的空格
-for subfolder_name in os.listdir(data_dir):
-    result_list = []
-    result = {}
+label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]  # 将所有训练的类别名用list保存
+# print(label_lines) ---> ['drowning', 'normal']
+result = {}
+
+for subfolder_name in os.listdir(data_dir):  # 遍历每个子文件夹
     subfolder_dir = pjoin(data_dir, subfolder_name)  # 子文件夹的路径
-    for i in os.listdir(subfolder_dir):
+    for i in os.listdir(subfolder_dir):  # 遍历每个图片
         image_path = pjoin(subfolder_dir, i)  # 得到每个图片的路径
         image_data = tf.gfile.FastGFile(image_path, 'rb').read()  # 读取图片
-        with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:  # 解析训练结果数据
+        # 解析训练结果数据
+        with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
             _ = tf.import_graph_def(graph_def, name='')
@@ -24,8 +27,15 @@ for subfolder_name in os.listdir(data_dir):
             predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})  # 生成预测数据
             top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]  # 按置信度排序
             # 输出每个成绩
-            print('文件夹：'+str(subfolder_name)+' 图片：'+str(i))
+            result_name = './' + str(subfolder_name) + '/' + str(i)
+            result_list = []
             for node_id in top_k:
                 human_string = label_lines[node_id]
                 score = predictions[0][node_id]
-                print('%s (score = %.5f)' % (human_string, score))
+                result_list.append(str('%s (score = %.5f)' % (human_string, score)))
+            result[result_name] = result_list
+            print(result_name, result_list)
+json_str = json.dumps(result, indent=4)
+# print(json_str)
+with open('test_result.json', 'w') as json_file:
+    json_file.write(json_str)
